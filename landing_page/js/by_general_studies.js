19b4273c35098a7da5a1e8ac4560f9c4b8144b53
daniellanev <nevies.daniella@gmail.com>
2017-06-15 14:20:15 +0300

    edu pages

---
+++ landing_page/js/by_general_studies.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('#show1').hide();
+	$('#show2').hide();
+	$('#show3').hide();
+	$('#show4').hide();
+	$('#show5').hide();
+	$('#show6').hide();
+	$('#show7').hide();
+	$('#show8').hide();
+
+    $('#sec1').click(function(){
+    	$('#show1').slideToggle(1500);
+    });
+    $('#sec2').click(function(){
+    	$('#show2').slideToggle(1500);
+    });
+    $('#sec3').click(function(){
+    	$('#show3').slideToggle(1500);
+    });
+    $('#sec4').click(function(){
+    	$('#show4').slideToggle(1500);
+    });
+    $('#sec5').click(function(){
+    	$('#show5').slideToggle(1500);
+    });
+    $('#sec6').click(function(){
+    	$('#show6').slideToggle(1500);
+    });
+    $('#sec7').click(function(){
+    	$('#show7').slideToggle(1500);
+    });
+    $('#sec8').click(function(){
+    	$('#show8').slideToggle(1500);
+    });
+});
+
+
+
+
+
+
+
+
+
+
